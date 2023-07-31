94a2b997c68de0ad676a6262d201fe052ea24dab
Tuan Anh Pham <tuananh@hotmail.se>
2023-07-31 15:08:25 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["./public/**/*.html", "./public/**/*.js"],
+  theme: {
+    extend: {
+      height: {
+        custom: "620px",
+      },
+      spacing: {
+        "40-c": "40rem",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
